0cfb2711470f57c4c220cc2cd7b66922dedd73bb
Sveta <lana.yurkevich@icloud.com>
2023-04-11 22:54:20 +0100

    Initial Commit

---
+++ Number guess/Number_guessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_guessApp.swift
+//  Number guess
+//
+//  Created by Svetlana Yurkevich on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Number_guessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
